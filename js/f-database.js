7bc8965c8768a29f01f4fefec095305a8209a82a
wuyupeng <15650733327@163.com>
2023-04-03 22:45:50 +0800

    数据整理

--- js/f-database.js
+++ js/f-database.js
@@ -11,18 +11,51 @@ export default {
         'https://s2.loli.net/2023/04/02/I8mptXkjU7uwTo6.jpg',
       ]
     },
+    {
+      name: '汽服POS端',
+      desc: '用于油站员工核销洗车券，以及统计日核销和月核销等相关功能',
+      coverImg: 'https://s2.loli.net/2023/04/02/SB1yknotZu8q9iN.jpg',
+      delImgs: [
+        'https://s2.loli.net/2023/04/02/SB1yknotZu8q9iN.jpg',
+        'https://s2.loli.net/2023/04/02/JhBMRnpIToewDaU.jpg',
+        'https://s2.loli.net/2023/04/02/V82mCWMbeJodkta.jpg',
+        'https://s2.loli.net/2023/04/02/I8mptXkjU7uwTo6.jpg',
+      ]
+    },
+    {
+      name: '手持中台',
+      desc: '用于油站员工核销洗车券，以及统计日核销和月核销等相关功能',
+      coverImg: 'https://s2.loli.net/2023/04/02/SB1yknotZu8q9iN.jpg',
+      delImgs: [
+        'https://s2.loli.net/2023/04/02/SB1yknotZu8q9iN.jpg',
+        'https://s2.loli.net/2023/04/02/JhBMRnpIToewDaU.jpg',
+        'https://s2.loli.net/2023/04/02/V82mCWMbeJodkta.jpg',
+        'https://s2.loli.net/2023/04/02/I8mptXkjU7uwTo6.jpg',
+      ]
+    },
+    {
+      name: '游戏中心',
+      desc: '配合H5使用的游戏中心项目，支持大转盘、刮刮乐、红包雨、划龙舟等多种游戏',
+      coverImg: 'https://s2.loli.net/2023/04/02/SB1yknotZu8q9iN.jpg',
+      delImgs: [
+        'https://s2.loli.net/2023/04/02/SB1yknotZu8q9iN.jpg',
+        'https://s2.loli.net/2023/04/02/JhBMRnpIToewDaU.jpg',
+        'https://s2.loli.net/2023/04/02/V82mCWMbeJodkta.jpg',
+        'https://s2.loli.net/2023/04/02/I8mptXkjU7uwTo6.jpg',
+      ]
+    },
   ],
-  // 'APP项目223': [
-  //   {
-  //     name: '加油河南',
-  //     desc: '为河南石化搭建的一款集商城、商城、导航等多种功能汇集的APP。',
-  //     coverImg: 'https://s2.loli.net/2023/04/02/SB1yknotZu8q9iN.jpg',
-  //     delImgs: [
-  //       'https://s2.loli.net/2023/04/02/SB1yknotZu8q9iN.jpg',
-  //       'https://s2.loli.net/2023/04/02/JhBMRnpIToewDaU.jpg',
-  //       'https://s2.loli.net/2023/04/02/V82mCWMbeJodkta.jpg',
-  //       'https://s2.loli.net/2023/04/02/I8mptXkjU7uwTo6.jpg',
-  //     ]
-  //   },
-  // ]
+  '小程序&公众号': [
+    {
+      name: '加油河南',
+      desc: '为河南石化搭建的一款集商城、商城、导航等多种功能汇集的APP。',
+      coverImg: 'https://s2.loli.net/2023/04/02/SB1yknotZu8q9iN.jpg',
+      delImgs: [
+        'https://s2.loli.net/2023/04/02/SB1yknotZu8q9iN.jpg',
+        'https://s2.loli.net/2023/04/02/JhBMRnpIToewDaU.jpg',
+        'https://s2.loli.net/2023/04/02/V82mCWMbeJodkta.jpg',
+        'https://s2.loli.net/2023/04/02/I8mptXkjU7uwTo6.jpg',
+      ]
+    },
+  ]
 }
